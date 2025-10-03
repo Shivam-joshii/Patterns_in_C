3170a7ef07a42570e0720a5e5c93fcc67c7b0740
Shivam-joshii <shivam.jooshii@gmail.com>
2025-10-03 22:06:50 +0530

    5.c

---
+++ 5.c
@@ -0,0 +1,29 @@
+/* 
+1
+12
+123
+1234
+12345
+123456
+1234567
+ number triangle of any number .
+       */   
+#include <stdio.h>
+
+int main()
+{ 
+   int n ;
+   printf("number triangle");
+   scanf("%d", &n);
+    // The loop for printing pattern. 
+
+    for(int i =1 ; i <= n; i++)
+    {
+        for (int j = 1 ; j<= i; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
